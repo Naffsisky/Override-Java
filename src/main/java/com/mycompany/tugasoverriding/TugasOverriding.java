27567394108182dffacc67ebca541710a7f84669
naffsvn <naffsvn@protonmail.com>
2023-05-13 00:58:47 +0700

    v1.1

--- src/main/java/com/mycompany/tugasoverriding/TugasOverriding.java
+++ src/main/java/com/mycompany/tugasoverriding/TugasOverriding.java
@@ -12,18 +12,51 @@ import java.util.Scanner;
 public class TugasOverriding {
 
     public static void main(String[] args) {
-        Pasien pasien1 = new Pasien("Dhany", 18);
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukan nama Pasien : ");
+        String namaPasien = input.nextLine();
+        System.out.print("Masukan umur Pasien : ");
+        int umurPasien = input.nextInt();
+        input.nextLine();
+        
+        System.out.print("Masukan nama Perawat : ");
+        String namaPerawat = input.nextLine();
+        System.out.print("Masukan umur Perawat : ");
+        int umurPerawat = input.nextInt();
+        input.nextLine();
+        System.out.print("Masukan skill Perawat : ");
+        String skillPerawat = input.nextLine();
+        System.out.print("Masukan pengalaman Perawat : ");
+        int pengalamanPerawat = input.nextInt();
+        input.nextLine();
+      
+        System.out.print("Masukan nama Dokter : ");
+        String namaDokter = input.nextLine();
+        System.out.print("Masukan umur Dokter : ");
+        int umurDokter = input.nextInt();
+        input.nextLine();
+        System.out.print("Masukan skill Dokter : ");
+        String skillDokter = input.nextLine();
+        System.out.print("Masukan spesialis Dokter : ");
+        String spesialisDokter = input.nextLine();
+        System.out.print("Masukan pengalaman Dokter : ");
+        int pengalamanDokter = input.nextInt();
+        input.nextLine();
+        
+//        Output
+        Pasien pasien1 = new Pasien(namaPasien, umurPasien);
         pasien1.perkenalan();
         pasien1.role();
         System.out.println("----");
-        Perawat perawat1 = new Perawat("Rena", 19, "Merawat");
+        Perawat perawat1 = new Perawat(namaPerawat, umurPerawat, skillPerawat);
         perawat1.perkenalan();
         perawat1.role();
-        perawat1.role(7);
+        perawat1.role(pengalamanPerawat);
         System.out.println("----");
-        Dokter dokter1 = new Dokter("Prinaf", 20, "Organ dalam", "Bedah");
+        Dokter dokter1 = new Dokter(namaDokter, umurDokter, skillDokter, spesialisDokter);
         dokter1.perkenalan();
         dokter1.role();
-        dokter1.role(10);
+        dokter1.role(pengalamanDokter);
     }
 }
